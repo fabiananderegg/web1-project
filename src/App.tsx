06c8fc76a6d6962e8422913cac398276f85d7dc1
janpfer <jan-pfiffner@hotmail.com>
2021-10-04 18:55:41 -0700

    UI - first trial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" >
+        <div className="Header-text">
+          Todo App
+        </div>
       </header>
+      <div className="App-body">
+          <div className="Task-input">
+            <div className="Task-input-text">
+              <input className="Inp-input-text" type="text">
+
+              </input>
+              <button className="Btn-confirm-input">
+                Hinzufügen
+              </button>
+            </div>
+            <div className="Task-input-filter">
+              <input className="Cb-filter-tasks" type="checkbox"/>
+              <div> Alle anzeigen</div>
+            </div>
+          </div>
+          <div className="Task-list">
+            <div className="firstTask">
+              <input className="Cb-first-task" type="checkbox"/>
+              <button className="Btn-first-task-one"> </button>
+              <div>Input text</div>
+              <button> Löschen </button>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
